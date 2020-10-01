8218a2ac25dcc84b400c8e8a4d798e793cf4f3e8
root <root@f5fe811094bc>
2020-10-01 02:55:45 +0000

    Level 1 Module 0 9/30/2020

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,36 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	
 	public void run() {
 		
+		Robot rob = new Robot();
+		rob.setX(100);
+		rob.setY(500);
+		rob.setSpeed(10);
+		rob.penDown();
+		rob.setPenWidth(3);
+		large(rob);
+		
 	}
+	
+	public void large(Robot x) {
+		for(int i = 0; i < 10; i++) {
+			
+			x.move(100);
+			x.turn(90);
+			x.move(20);
+			x.turn(90);
+			x.move(100);
+			x.turn(-90);
+			x.setPenColor(Color.green);
+			x.move(20);
+			x.turn(-90);
+		}
+	}
+	
 }
